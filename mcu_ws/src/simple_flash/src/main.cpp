03c3ada149b543e3793d9b4eb07c294a84a43af3
Tal Avital <108590031+TAvital04@users.noreply.github.com>
2026-03-30 14:20:20 -0400

    tal's first flashing in the library

---
+++ mcu_ws/src/simple_flash/src/main.cpp
@@ -0,0 +1,16 @@
+#include <Arduino.h>
+
+#define LED 13
+
+void setup() {
+  Serial.begin(9600);
+
+  pinMode(LED, OUTPUT);
+}
+
+void loop() {
+  digitalWrite(LED, HIGH);
+  delay(1000);
+  digitalWrite(LED, LOW);
+  delay(1000);
+}
