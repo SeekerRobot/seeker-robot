c1f109b8229ffb42cd8e59e3d37a802553820d94
Tal Avital <108590031+TAvital04@users.noreply.github.com>
2026-04-04 10:10:21 -0400

    configure pins

--- mcu_ws/src/test_sub_cam/include/camera_pins.h
+++ mcu_ws/src/test_sub_cam/include/camera_pins.h
@@ -1,34 +1,36 @@
+// For OV3660, should be hotswappable
+
 #ifndef CAMERA_PINS_H
 #define CAMERA_PINS_H
 
 #include "esp_camera.h"
 
 // Pins that perform power/reset operations
-#define CAM_PIN_PWDN 32   // Power down pin
-#define CAM_PIN_RESET -1  // Reset is performed through software
+#define CAM_PIN_PWDN  8   // Power down pin
+#define CAM_PIN_RESET 6  // Reset is performed through software
 
 // Pin that drives the camera's internal clock
-#define CAM_PIN_XCLK 0
+#define CAM_PIN_XCLK 13
 
 // Pins that send config messages
-#define CAM_PIN_SIOD 26  // Data wire used to send the messages
-#define CAM_PIN_SIOC 27  // Clock wire used to synchronize message timing
+#define CAM_PIN_SIOD 3  // Data wire used to send the messages
+#define CAM_PIN_SIOC 5  // Clock wire used to synchronize message timing
 
 // Mega highway of pins for sending the images
 // Each digit of each 8-bit word is moved by each pin, 1 pulse at a time
-#define CAM_PIN_D7 35
-#define CAM_PIN_D6 34
-#define CAM_PIN_D5 39
-#define CAM_PIN_D4 36
-#define CAM_PIN_D3 21
-#define CAM_PIN_D2 19
-#define CAM_PIN_D1 18
-#define CAM_PIN_D0 5
+#define CAM_PIN_D7 12
+#define CAM_PIN_D6 14
+#define CAM_PIN_D5 16
+#define CAM_PIN_D4 18
+#define CAM_PIN_D3 20
+#define CAM_PIN_D2 22
+#define CAM_PIN_D1 21
+#define CAM_PIN_D0 19
 
 // Clock wires
-#define CAM_PIN_VSYNC 25  // Marks the start of an image
-#define CAM_PIN_HREF 23   // On high while a row is being sent
-#define CAM_PIN_PCLK 22   // On high when a new word should be read
+#define CAM_PIN_VSYNC 7  // Marks the start of an image
+#define CAM_PIN_HREF  9   // On high while a row is being sent
+#define CAM_PIN_PCLK  17   // On high when a new word should be read
 
 // Camera config data structure
 static camera_config_t camera_config = {
